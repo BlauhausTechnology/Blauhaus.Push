c77986f3b3b9e5e15d5c5fd91453e1ffef5b6519
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-12-10 20:02:52 +0200

    no message

--- src/Blauhaus.Push.TestHelpers/MockBuilders/PushNotificationsServerServiceMockBuilder.cs
+++ src/Blauhaus.Push.TestHelpers/MockBuilders/PushNotificationsServerServiceMockBuilder.cs
@@ -1,10 +1,53 @@
-﻿using Blauhaus.Push.Abstractions.Server;
+﻿using System;
+using System.Linq.Expressions;
+using System.Threading;
+using Blauhaus.Errors;
+using Blauhaus.Push.Abstractions.Common.Notifications;
+using Blauhaus.Push.Abstractions.Server;
+using Blauhaus.Responses;
 using Blauhaus.TestHelpers.MockBuilders;
+using Moq;
 
 namespace Blauhaus.Push.TestHelpers.MockBuilders
 {
     public class PushNotificationsServerServiceMockBuilder : BaseMockBuilder<PushNotificationsServerServiceMockBuilder, IPushNotificationsServerService>
     {
-   
+        public PushNotificationsServerServiceMockBuilder Where_UpdateDeviceRegistrationAsync_returns(IDeviceRegistration result)
+        {
+            Mock.Setup(x => x.UpdateDeviceRegistrationAsync(Any<IDeviceRegistration>(), Any<IPushNotificationsHub>(), AnyToken))
+                .ReturnsAsync(Response.Success(result));
+            return this;
+        }
+        
+        public PushNotificationsServerServiceMockBuilder Where_UpdateDeviceRegistrationAsync_fails(Error error)
+        {
+            Mock.Setup(x => x.UpdateDeviceRegistrationAsync(Any<IDeviceRegistration>(), Any<IPushNotificationsHub>(), AnyToken))
+                .ReturnsAsync(Response.Failure<IDeviceRegistration>(error));
+            return this;
+        }
+
+        public PushNotificationsServerServiceMockBuilder Where_LoadRegistrationForUserDeviceAsync_returns(IDeviceRegistration result)
+        {
+            Mock.Setup(x => x.LoadRegistrationForUserDeviceAsync(AnyString, AnyString, Any<IPushNotificationsHub>(), AnyToken))
+                .ReturnsAsync(Response.Success(result));
+            return this;
+        }
+        
+        public PushNotificationsServerServiceMockBuilder Where_LoadRegistrationForUserDeviceAsync_fails(Error error)
+        {
+            Mock.Setup(x => x.LoadRegistrationForUserDeviceAsync(AnyString, AnyString, Any<IPushNotificationsHub>(), AnyToken))
+                .ReturnsAsync(Response.Failure<IDeviceRegistration>(error));
+            return this;
+        }
+        
+        public PushNotificationsServerServiceMockBuilder Where_DeregisterUserDeviceAsync_returns(Response result)
+        {
+            Mock.Setup(x => x.DeregisterUserDeviceAsync(AnyString, AnyString, Any<IPushNotificationsHub>(), AnyToken))
+                .ReturnsAsync(result);
+            return this;
+        }
+
+         
+
     }
 }
