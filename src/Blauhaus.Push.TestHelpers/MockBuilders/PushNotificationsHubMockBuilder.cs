b56a23d53bf40280cb251cbb9865626fdd77abe6
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-12-10 19:30:00 +0200

    no message

---
+++ src/Blauhaus.Push.TestHelpers/MockBuilders/PushNotificationsHubMockBuilder.cs
@@ -0,0 +1,15 @@
+﻿using AutoFixture;
+using Blauhaus.Push.Abstractions.Server;
+using Blauhaus.TestHelpers.MockBuilders;
+
+namespace Blauhaus.Push.TestHelpers.MockBuilders
+{
+    public class PushNotificationsHubMockBuilder : BaseMockBuilder<PushNotificationsHubMockBuilder, IPushNotificationsHub>
+    {
+        public PushNotificationsHubMockBuilder()
+        {
+            With(x => x.NotificationHubConnectionString, MyFixture.Create<string>());
+            With(x => x.NotificationHubName, MyFixture.Create<string>());
+        }
+    }
+}
