be6b7c5d57d6ca30d03a118d4669ac164d7169c2
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2022-03-31 15:41:03 +0200

    no message

--- src/Blauhaus.Push.Client.Maui/.Ioc/ServiceCollectionExtensions.cs
+++ src/Blauhaus.Push.Client.Maui/.Ioc/ServiceCollectionExtensions.cs
@@ -1,13 +1,22 @@
 ﻿using Blauhaus.Push.Abstractions.Client;
+using Blauhaus.Push.Client.Maui.Common;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Blauhaus.Push.Client.Maui.Ioc;
 
 public static class ServiceCollectionExtensions
 {
+
     public static IServiceCollection AddPushNotificationsClient(this IServiceCollection services, Action<PushNotificationsClientOptions> options)
     {
+        return services.AddPushNotificationsClient<EmptyPushNotificationTapHandler>(options);
+    }
+
+    public static IServiceCollection AddPushNotificationsClient<TTapHandler>(this IServiceCollection services, Action<PushNotificationsClientOptions> options) where TTapHandler : class, IPushNotificationTapHandler
+    {
         services.Configure(options);
+
+        services.TryAddSingleton<IPushNotificationTapHandler, TTapHandler>();
 
 #if IOS
         services.TryAddSingleton<IosPushNotificationHandler>();
---
+++ src/Blauhaus.Push.Client.Maui/Common/EmptyPushNotificationTapHandler.cs
@@ -0,0 +1,25 @@
+﻿using Blauhaus.Analytics.Abstractions;
+using Blauhaus.Analytics.Abstractions.Service;
+using Blauhaus.Push.Abstractions.Client;
+using Blauhaus.Push.Abstractions.Common.Notifications;
+using Microsoft.Extensions.Logging;
+
+namespace Blauhaus.Push.Client.Maui.Common
+{
+    public class EmptyPushNotificationTapHandler : IPushNotificationTapHandler
+    {
+        private readonly IAnalyticsLogger<EmptyPushNotificationTapHandler> _logger;
+
+        public EmptyPushNotificationTapHandler(IAnalyticsLogger<EmptyPushNotificationTapHandler> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public Task HandleTapAsync(IPushNotification tappedNotification)
+        {
+            _logger.LogTrace("No Push Notification Tap Handler configured");
+            return Task.CompletedTask;
+        }
+    }
+}
