82afdc63702ace3e72a651d78561b99fdeba31da
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2023-01-06 14:06:29 +0200

    no message

---
+++ run.bat
@@ -0,0 +1,5 @@
+dotnet publish src/Blauhaus.Push.sln -f net7.0 -c Release 
+dotnet publish src/Blauhaus.Push.sln -f net7.0-ios -c Release 
+dotnet publish src/Blauhaus.Push.sln -f net7.0-android -c Release 
+dotnet publish src/Blauhaus.Push.sln -f net7.0-windows10.0.19041 
+dotnet publish src/Blauhaus.Push.sln -f netstandard2.0 -c Release 
