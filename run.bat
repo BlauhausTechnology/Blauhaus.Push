c91d5c2141441fd92fa176d1ed8feb53f84f60e1
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2023-01-06 14:34:42 +0200

    no message

--- run.bat
+++ run.bat
@@ -1,5 +1,8 @@
 dotnet publish ./src/Blauhaus.Push.Abstractions/Blauhaus.Push.Abstractions.csproj -f netstandard2.0 -c Release -o $(build.sourcesdirectory)
 dotnet publish ./src/Blauhaus.Push.Client/Blauhaus.Push.Client.csproj -f netstandard2.0 -c Release -o $(build.sourcesdirectory)
+dotnet publish ./src/Blauhaus.Push.Client/Blauhaus.Push.Client.csproj -f Xamarin.iOS10 -c Release -o $(build.sourcesdirectory)
+dotnet publish ./src/Blauhaus.Push.Client/Blauhaus.Push.Client.csproj -f MonoAndroid10.0 -c Release -o $(build.sourcesdirectory)
+dotnet publish ./src/Blauhaus.Push.Client/Blauhaus.Push.Client.csproj -f uap10.0.19041 -c Release -o $(build.sourcesdirectory)
 dotnet publish ./src/Blauhaus.Push.Client.Maui/Blauhaus.Push.Client.Maui.csproj -f net7.0 -c Release -o $(build.sourcesdirectory)
 dotnet publish ./src/Blauhaus.Push.Client.Maui/Blauhaus.Push.Client.Maui.csproj -f net7.0-ios -c Release -o $(build.sourcesdirectory)
 dotnet publish ./src/Blauhaus.Push.Client.Maui/Blauhaus.Push.Client.Maui.csproj -f net7.0-android -c Release -o $(build.sourcesdirectory)
