3b26ef7a34dbcf4fa227fbf6db437fe7bb12d305
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2023-07-28 10:37:46 +0200

    no message

--- src/Blauhaus.Push.Client.Maui/Platforms/iOS/IosPushNotificationsHandler.cs
+++ src/Blauhaus.Push.Client.Maui/Platforms/iOS/IosPushNotificationsHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Blauhaus.Analytics.Abstractions;
 using Blauhaus.Push.Abstractions.Client;
 using Blauhaus.Analytics.Abstractions.Service;
 using UIKit;
@@ -7,7 +8,8 @@ using Foundation;
 using UserNotifications;
 using Blauhaus.Analytics.Abstractions.Extensions;
 using Blauhaus.Push.Abstractions.Common;
-using Blauhaus.Push.Client.Maui.Ioc; 
+using Blauhaus.Push.Client.Maui.Ioc;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 // ReSharper disable once CheckNamespace
@@ -15,85 +17,84 @@ namespace Blauhaus.Push.Client.Maui
 {
     public class IosPushNotificationHandler
     {
-        private readonly IAnalyticsService _analyticsService;
+        private readonly IAnalyticsLogger<IosPushNotificationHandler> _logger;
         private readonly IPushNotificationsClientConfig _options;
         private readonly IosPushNotificationsClientService _pushNotificationsService;
 
         public IosPushNotificationHandler(
-            IAnalyticsService analyticsService, 
+            IAnalyticsLogger<IosPushNotificationHandler> logger, 
             IPushNotificationsClientService pushNotificationsService,
             IPushNotificationsClientConfig options)
         {
             _pushNotificationsService = (IosPushNotificationsClientService) pushNotificationsService;
-            _analyticsService = analyticsService;
+            _logger = logger;
             _options = options;
         }
 
 
         public async Task InitializeAsync(MauiUIApplicationDelegate appDelegate, NSDictionary options)
         {
+            
+            using var _ = _logger.BeginTimedScope(LogLevel.Debug, "iOs Push Notifications Initialization");
 
-            using (var _ = _analyticsService.StartTrace(this, "iOs Push Notifications Initialization"))
+            if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
             {
-                if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
-                {
-                    UNUserNotificationCenter.Current.RequestAuthorization(UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound,
-                        (granted, error) =>
+                UNUserNotificationCenter.Current.RequestAuthorization(UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound,
+                    (granted, error) =>
+                    {
+                        if (granted)
                         {
-                            if (granted)
+                            _logger.LogInformation("Permission granted for push notifications");
+                            appDelegate.InvokeOnMainThread(UIApplication.SharedApplication.RegisterForRemoteNotifications);
+                        }
+                        else
+                        {
+                            if(error?.Code != null)
                             {
-                                _analyticsService.TraceInformation(this, "Permission granted for push notifications");
-                                appDelegate.InvokeOnMainThread(UIApplication.SharedApplication.RegisterForRemoteNotifications);
+                                _logger.LogWarning("Failed to get permission for push notifications: {Error}", error.Code.ToString());
                             }
                             else
                             {
-                                if(error?.Code != null)
-                                {
-                                    _analyticsService.TraceError(this, "Failed to get permission for push notifications", "ErrorCode", error.Code.ToString());
-                                }
-                                else
-                                {
-                                    _analyticsService.TraceWarning(this, "Permission for push notifications not granted");
-                                }
+                                _logger.LogWarning("Permission for push notifications not granted");
                             }
-                        });
-                }
-                else if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
-                {
-                    var pushSettings = UIUserNotificationSettings.GetSettingsForTypes(UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound, new NSSet());
+                        }
+                    });
+            }
+            else if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
+            {
+                var pushSettings = UIUserNotificationSettings.GetSettingsForTypes(UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound, new NSSet());
 
-                    UIApplication.SharedApplication.RegisterUserNotificationSettings(pushSettings);
-                    UIApplication.SharedApplication.RegisterForRemoteNotifications();
-                }
-                else
-                {
-                    const UIRemoteNotificationType notificationTypes = UIRemoteNotificationType.Alert | UIRemoteNotificationType.Badge | UIRemoteNotificationType.Sound;
-                    UIApplication.SharedApplication.RegisterForRemoteNotificationTypes(notificationTypes);
-                }
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(pushSettings);
+                UIApplication.SharedApplication.RegisterForRemoteNotifications();
+            }
+            else
+            {
+                const UIRemoteNotificationType notificationTypes = UIRemoteNotificationType.Alert | UIRemoteNotificationType.Badge | UIRemoteNotificationType.Sound;
+                UIApplication.SharedApplication.RegisterForRemoteNotificationTypes(notificationTypes);
+            }
 
-                if (options != null)
+            if (options != null)
+            {
+                if (options.TryGetValue(new NSString("UIApplicationLaunchOptionsRemoteNotificationKey"), out var tapped))
                 {
-                    if (options.TryGetValue(new NSString("UIApplicationLaunchOptionsRemoteNotificationKey"), out var tapped))
-                    {
-                        _analyticsService.TraceVerbose(this, "Notification payload available for new app session");
-                        await HandleMessageReceivedAsync(UIApplicationState.Inactive, (NSDictionary) tapped);
-                    }
+                    _logger.LogInformation("Notification payload available for new app session");
+                    _logger.LogDebug("Payload: {UIApplicationLaunchOptionsRemoteNotificationKey}", tapped);
+                    await HandleMessageReceivedAsync(UIApplicationState.Inactive, (NSDictionary) tapped);
                 }
             }
-            
         }
 
         public async void HandleNewToken(NSData deviceToken)
         {
-            var tokenBytes = deviceToken.ToArray();
-            var tokenString = BitConverter.ToString(tokenBytes).Replace("-","");
+            byte[] tokenBytes = deviceToken.ToArray();
+            string tokenString = BitConverter.ToString(tokenBytes).Replace("-","");
 
             await _pushNotificationsService.UpdatePushNotificationServiceHandleAsync(tokenString);
         }
 
         public void HandleFailedRegistration(NSError deviceToken)
         {
-            _analyticsService.TraceError(this, PushErrors.InvalidDeviceRegistration);
+            _logger.LogError(PushErrors.InvalidDeviceRegistration);
         }
 
         public async Task HandleMessageReceivedAsync(UIApplicationState applicationState, NSDictionary userInfo)
